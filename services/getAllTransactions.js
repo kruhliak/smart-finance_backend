73e81944bb1067016fad9e7f8b1ad377251cc4f0
Stanislav Kruhliak <73795709+kruhliak@users.noreply.github.com>
2021-11-17 15:42:41 +0100

    Update getAllTransactions.js
    fix fn getAll

--- services/getAllTransactions.js
+++ services/getAllTransactions.js
@@ -3,7 +3,7 @@ const { Transaction } = require("../models");
 const getAllTransactions = async (req, res) => {
   const { _id: ownerId } = req.user;
 
-  const data = await Transaction.find({ ownerId });
+  const data = await Transaction.find({ owner: ownerId });
   return data;
 };
 
